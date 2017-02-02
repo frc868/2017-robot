66e20e030e2621dcc93d019b64db5460f791bee0
Caleb <karagenit@outlook.com>
2017-02-02 18:00:15 -0500

    Add RotateTurretToAngle Command

---
+++ src/org/usfirst/frc/team868/robot/commands/RotateTurretToAngle.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team868.robot.commands;
+
+import org.usfirst.frc.team868.robot.subsystems.TurretRotationSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RotateTurretToAngle extends Command {
+
+	TurretRotationSubsystem turret;
+	double angle;
+	
+    public RotateTurretToAngle(double absoluteAngle) {
+        turret = TurretRotationSubsystem.getInstance();
+        requires(turret);
+        angle = absoluteAngle; 
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	turret.setPosition(angle); //convert to encoder counts?
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
--- src/org/usfirst/frc/team868/robot/subsystems/TurretRotationSubsystem.java
+++ src/org/usfirst/frc/team868/robot/subsystems/TurretRotationSubsystem.java
@@ -10,28 +10,32 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class TurretRotationSubsystem extends Subsystem {
-	
+
 	private static TurretRotationSubsystem instance;
 	private CANTalon turretRotator;
-	
-    private TurretRotationSubsystem(){
-    	turretRotator = new CANTalon(RobotMap.Turret.TURRET_MOTOR);
-    	turretRotator.setInverted(RobotMap.Turret.IS_INVERTED);
-    	turretRotator.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-    }
-    
-    public void setPosition(double pos){
-    	turretRotator.setPosition(pos);
-    }
-    
-    public static TurretRotationSubsystem getInstance(){
-    	if(instance == null){
-    		instance = new TurretRotationSubsystem();
-    	}
-    	return instance;
-    }
 
-    public void initDefaultCommand() {
-    }
+	private TurretRotationSubsystem(){
+		turretRotator = new CANTalon(RobotMap.Turret.TURRET_MOTOR);
+		turretRotator.setInverted(RobotMap.Turret.IS_INVERTED);
+		turretRotator.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+	}
+
+	public void setPosition(double pos){
+		turretRotator.setPosition(pos); 
+	}
+
+	//TODO for a PID object should this have a setSetpoint?
+
+	//TODO setPower method?
+
+	public static TurretRotationSubsystem getInstance(){
+		if(instance == null){
+			instance = new TurretRotationSubsystem();
+		}
+		return instance;
+	}
+
+	public void initDefaultCommand() {
+	}
 }
 
