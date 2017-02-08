2350f132ddff543382db3c6e16e20a00a1c7b6d2
Evan <Evan@10.35.25.29>
2017-02-08 17:03:46 -0500

    Create AutonChooser and AutonLauncher

---
+++ src/org/usfirst/frc/team868/robot/commands/AutonChooser.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team868.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+/**
+ *
+ */
+public class AutonChooser {
+
+	private static AutonChooser instance;
+	
+	private static SendableChooser chooseStart;
+	private static SendableChooser chooseDoThis;
+	
+	public static AutonChooser getInstance() {
+		if(instance == null) instance = new AutonChooser();
+		return instance;
+	}
+    private AutonChooser() { 
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	setupDashboard();
+    }
+    
+    public static enum StartingPoint{
+    	B1, B2, B3, R1, R2, R3
+    }
+    
+    public static enum DoThis {
+    	CROSS_BASE, DROP_GEAR, HOPPER
+    }
+    
+    public static StartingPoint getStart() {
+    	return ((StartingPoint) chooseStart.getSelected());
+    }
+    
+    public static DoThis getDoThis() {
+    	return ((DoThis) chooseDoThis.getSelected());
+    }
+    
+    public void setupDashboard() {
+    	chooseStart = new SendableChooser();
+    		chooseStart.addObject("B1", StartingPoint.B1);
+    		chooseStart.addObject("B2", StartingPoint.B2);
+    		chooseStart.addObject("B3", StartingPoint.B3);
+    		chooseStart.addObject("R1", StartingPoint.R1);
+    		chooseStart.addObject("R2", StartingPoint.R2);
+    		chooseStart.addObject("R3", StartingPoint.R3);
+    		
+    	chooseDoThis = new SendableChooser();
+    		chooseDoThis.addObject("A: Cross the Baseline", DoThis.CROSS_BASE);
+    		chooseDoThis.addObject("B: Drop off Gear", DoThis.DROP_GEAR);
+    		chooseDoThis.addObject("C: Shoot Balls", DoThis.HOPPER);
+    }
+
+}
---
+++ src/org/usfirst/frc/team868/robot/commands/AutonLauncher.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team868.robot.commands;
+
+import org.usfirst.frc.team868.robot.commands.AutonChooser;
+import org.usfirst.frc.team868.robot.commands.AutonChooser.DoThis;
+import org.usfirst.frc.team868.robot.commands.auton.DropOffGearAutonCommandGroup;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutonLauncher extends CommandGroup {
+
+    public AutonLauncher() {
+        DoThis selected = AutonChooser.getDoThis();
+        switch(selected) {
+        	case CROSS_BASE:
+        		break;
+        	case DROP_GEAR:
+        		new DropOffGearAutonCommandGroup().start();
+        		break;
+        	case HOPPER: 
+        		break;
+        }
+        	
+    }
+    
+    
+}
