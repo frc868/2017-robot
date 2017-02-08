790a76d3ddd0150493e488705252559cf48ea99d
Clayton <cjdetke@yahoo.com>
2017-02-08 17:48:19 -0500

    Added javadocs and updateSD method to gear collector subsystem

--- src/org/usfirst/frc/team868/robot/subsystems/GearCollectorSubsystem.java
+++ src/org/usfirst/frc/team868/robot/subsystems/GearCollectorSubsystem.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team868.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GearCollectorSubsystem extends Subsystem {
 
@@ -14,32 +15,51 @@ public class GearCollectorSubsystem extends Subsystem {
 	@Override
 	protected void initDefaultCommand() {}
 
+	/**
+	 * Constructor
+	 */
 	private GearCollectorSubsystem() {
 		sol = new Solenoid(RobotMap.GearCollector.GEAR_SOLENOID);
 	}
-	
+	/**
+	 * Normal get instance method.
+	 */
 	public static GearCollectorSubsystem getInstance() {
 		return instance == null ? instance = new GearCollectorSubsystem() : instance;
 	}
-	
+	/**
+	 * Set the gear collector to either: open = true, or closed = false.
+	 */
 	public void setGearCollector(boolean on) {
 		state = on;
 		sol.set(state);
 	}
-	
+	/**
+	 * Opens the gear collector, if not already open.
+	 */
 	public void setGearCollectorOpen() {
 		setGearCollector(true);
 	}
-	
+	/**
+	 * Closes the gear collector, if not already closed.
+	 */
 	public void setGearCollectorClosed() {
 		setGearCollector(false);
 	}
-	
+	/**
+	 * Switches the gear collector between open and closed states.
+	 */
 	public void toggleGearCollector() {
 		setGearCollector(!state);
 	}
-	
+	/**
+	 * Returns whether or not the gear collector is open.
+	 */
 	public boolean isGearCollectorOpen() {
 		return state;
+	}
+	
+	public void updateSD(){
+		SmartDashboard.putBoolean("Gear Collector", state);
 	}
 }
