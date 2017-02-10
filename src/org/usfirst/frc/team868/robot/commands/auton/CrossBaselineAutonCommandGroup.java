efb8b72eeccd480027e76e4a0fe093565fb7dc0e
andrewgostomelsky <andrewgostomelsky@CHS-C105-C19W.ccs.k12.in.us>
2017-02-10 18:04:58 -0500

    Add Simple Auton Sequences

--- src/org/usfirst/frc/team868/robot/commands/auton/CrossBaselineAutonCommandGroup.java
+++ src/org/usfirst/frc/team868/robot/commands/auton/CrossBaselineAutonCommandGroup.java
@@ -1,8 +1,11 @@
 package org.usfirst.frc.team868.robot.commands.auton;
 
 import org.usfirst.frc.team868.robot.commands.AutonChooser.StartingPoint;
+import org.usfirst.frc.team868.robot.commands.groups.FeedAndShootCommandGroup;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveAndTurn;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveDistanceCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.drive.TurnToAngle;
+import org.usfirst.frc.team868.robot.commands.subsystems.gear.GearReleaseCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -20,12 +23,26 @@ public class CrossBaselineAutonCommandGroup extends CommandGroup {
 	    		addSequential(new DriveDistanceCommand(487.68));
 	    		break;
 	    	case B2:
+	    		addSequential(new DriveDistanceCommand(-40));
+	    		addSequential(new TurnToAngle(90));
+	    		addSequential(new DriveDistanceCommand(409));
+	    		addSequential(new TurnToAngle(90));
+	    		addSequential(new DriveDistanceCommand(-50));
+	    		addSequential(new DriveDistanceCommand(200));
+	    		addSequential(new FeedAndShootCommandGroup());
+	    		break;
 	    	case R2:
-	    		addSequential(new DriveAndTurn(100, true));
-	    		addSequential(new DriveDistanceCommand(1000));
+	    		addSequential(new DriveDistanceCommand(-40));
+	    		addSequential(new TurnToAngle(90));
+	    		addSequential(new DriveDistanceCommand(409));
+	    		addSequential(new TurnToAngle(90));
+	    		addSequential(new DriveDistanceCommand(-50));
+	    		addSequential(new DriveDistanceCommand(200));
+	    		addSequential(new FeedAndShootCommandGroup());
 	    		break;
 	    	default: 
 	    		break;
+	    		return true;
     	}
     }
 }
