fe53e6641f5f687b6ebe487b87f830175f7fd88a
Caleb <karagenit@outlook.com>
2017-02-10 15:58:52 -0500

    Clean Up CrossBaseline Command Group

--- src/org/usfirst/frc/team868/robot/commands/auton/CrossBaselineAutonCommandGroup.java
+++ src/org/usfirst/frc/team868/robot/commands/auton/CrossBaselineAutonCommandGroup.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team868.robot.commands.auton;
 
-import org.usfirst.frc.team868.robot.commands.AutonChooser.DoThis;
 import org.usfirst.frc.team868.robot.commands.AutonChooser.StartingPoint;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveAndTurn;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveDistanceCommand;
@@ -13,12 +12,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CrossBaselineAutonCommandGroup extends CommandGroup {
 
     public CrossBaselineAutonCommandGroup(StartingPoint selected) {
-    	if(selected == StartingPoint.B1 || selected == StartingPoint.B3 || selected == StartingPoint.R1 || selected == StartingPoint.R3) {
-    		addSequential(new DriveDistanceCommand(487.68));
-    	}
-    	if(selected == StartingPoint.B2 || selected == StartingPoint.R2) {
-    		addSequential(new DriveAndTurn(100, true));
-    		addSequential(new DriveDistanceCommand(1000));
+    	switch(selected) {
+	    	case B1:
+	    	case B3:
+	    	case R1:
+	    	case R3:
+	    		addSequential(new DriveDistanceCommand(487.68));
+	    		break;
+	    	case B2:
+	    	case R2:
+	    		addSequential(new DriveAndTurn(100, true));
+	    		addSequential(new DriveDistanceCommand(1000));
+	    		break;
+	    	default: 
+	    		break;
     	}
     }
 }
