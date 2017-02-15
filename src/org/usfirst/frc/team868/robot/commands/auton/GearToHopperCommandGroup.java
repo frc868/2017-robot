5716839cc87090eb4686bad74fc6d23d17348728
andrewgostomelsky <andrewgostomelsky@gmail.com>
2017-02-15 16:36:56 -0500

    GearToHopper GearToNeutral Command Groups added

---
+++ src/org/usfirst/frc/team868/robot/commands/auton/GearToHopperCommandGroup.java
@@ -0,0 +1,83 @@
+package org.usfirst.frc.team868.robot.commands.auton;
+
+import org.usfirst.frc.team868.robot.commands.AutonChooser.StartingPoint;
+import org.usfirst.frc.team868.robot.commands.groups.FeedAndShootCommandGroup;
+import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveDistanceCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.drive.TurnToAngle;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class GearToHopperCommandGroup extends CommandGroup {
+
+    public GearToHopperCommandGroup(StartingPoint selected) {
+    	switch(selected) {
+    	case B1:
+    		addSequential(new DriveDistanceCommand(-75));
+    		addSequential(new TurnToAngle(45));
+    		addSequential(new DriveDistanceCommand(300));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(600));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(250));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(220));
+    		break;
+    	case B2:
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(310));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(120));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new DriveDistanceCommand(-100));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new FeedAndShootCommandGroup());
+    		break;
+    	case B3:
+    		addSequential(new TurnToAngle(-135));
+    		addSequential(new DriveDistanceCommand(230));
+    		addSequential(new DriveDistanceCommand(-100));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new FeedAndShootCommandGroup());
+    		break;
+    	case R1:
+    		addSequential(new DriveDistanceCommand(-75));
+    		addSequential(new TurnToAngle(-45));
+    		addSequential(new DriveDistanceCommand(300));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(600));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(250));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(220));
+    		break;
+    	case R2:
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(310));
+    		addSequential(new TurnToAngle(-90));
+    		addSequential(new DriveDistanceCommand(120));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new DriveDistanceCommand(-100));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new FeedAndShootCommandGroup());
+    		break;
+    	case R3:
+    		addSequential(new TurnToAngle(135));
+    		addSequential(new DriveDistanceCommand(230));
+    		addSequential(new DriveDistanceCommand(-100));
+    		addSequential(new TurnToAngle(90));
+    		addSequential(new DriveDistanceCommand(100));
+    		addSequential(new FeedAndShootCommandGroup());
+    		break;
+    	default: 
+    		break;
+    	}
+    }
+}
