b842d84188db8bd9c1fd8f348b696b63a3b7a248
Clayton <cjdetke@yahoo.com>
2017-02-16 16:51:57 -0500

    Updated and organized RobotMap.

--- src/org/usfirst/frc/team868/robot/RobotMap.java
+++ src/org/usfirst/frc/team868/robot/RobotMap.java
@@ -11,6 +11,15 @@ import edu.wpi.first.wpilibj.SerialPort;
 
 public interface RobotMap {
 	
+	public interface Climber {
+		//PORTS:
+		final int CLIMBER_MOTOR = 10;
+		
+		//VALUES:
+		final boolean IS_INVERTED = false;
+		final double CLIMBER_SPEED = 0.8;
+	}
+	
 	public interface Drive {
 		//PORTS:
 		final int LEFT_MOTOR = 9;
@@ -26,6 +35,79 @@ public interface RobotMap {
 		final double SPEED = 0.8; //default drive speed
 		final double COUNTS_PER_CM = 1; //TODO calculate
 		final double CM_PER_COUNT = 1;
+	}
+	
+	public interface Feeder {
+		//PORTS:
+		final int AGITATOR_MOTOR = 7;
+		final int CONVEYOR_MOTOR = 6;
+		final int BEAM_BREAK_PORT = 0;
+		
+		//VALUES:
+		final boolean AGITATOR_IS_INVERTED = false;
+		final double AGITATOR_SPEED = 0.8;
+		
+		final boolean CONVEYOR_IS_INVERTED = false;
+		final double CONVEYOR_SPEED = 0.8;
+	}
+	
+	public interface Flashlight {
+		//PORTS:
+		final int GEAR_LIGHT_PORT = 1;
+		final int SHOOT_LIGHT_PORT = 0;
+	}
+	
+	public interface GearCollector {
+		//PORTS:
+		final int GEAR_SOLENOID = 0;
+		final int GEAR_DETECTOR_ANALOG_PORT = 0;
+		
+		//VALUES:
+		final double DETECTION_VOLTAGE = .1;
+	}
+	
+	public static class Joystick {
+		//PORTS:
+		public static final int PORT_NUMBER = 0;
+		public static final int LEFT_Y = 1;
+		public static final int RIGHT_Y = 5;
+		public static final int LEFT_X = 0;
+		public static final int RIGHT_X = 4;
+	}
+	
+	public interface LEDs {
+		//PORTS:
+		final int DIO_1_PORT = 0;
+		final int DIO_2_PORT = 1;
+		final int DIO_GEAR_PORT = 2;
+	}
+	
+	public interface LIDAR {
+		//PORTS:
+		final SerialPort.Port PORT = SerialPort.Port.kUSB;
+		
+		//VALUES:
+		final int BAUD = 9600;
+	}
+	
+	public interface Pixy {
+		//PORTS:
+		final SerialPortType IR_PORT_TYPE = SerialPortType.SERIAL_MXP;
+		final SerialPortType COLOR_PORT_TYPE = SerialPortType.I2C_ONBOARD;
+		final int IR_I2C_VALUE = 0x5; // Only needed if using I2C
+		final int COLOR_I2C_VALUE = 0x5;
+		
+		//VALUES:
+		final int CAM_WIDTH = 318; //in pixels
+		final int CAM_HEIGHT = 198;//in pixels
+		final int CAM_X_ANGLE = 80;//in degrees
+		final int CAM_Y_ANGLE = 40;//in degrees
+		final int BAUDRATE = 19200;
+	}
+	
+	/** Serial port types that make it so that serial objects only need their port value changed in RobotMap */
+	public static enum SerialPortType{
+		SERIAL_ONBOARD, SERIAL_MXP, I2C_ONBOARD, I2C_MXP
 	}
 	
 	public interface Shoot {
@@ -52,75 +134,5 @@ public interface RobotMap {
 		final int REVERSE_LIMIT = -90;
 		final double COUNTS_PER_DEGREE = 1;
 		final double DEGREES_PER_COUNT = 1/COUNTS_PER_DEGREE;
-	}
-	
-	public interface Feeder {
-		//PORTS:
-		final int AGITATOR_MOTOR = 7;
-		
-		final int CONVEYOR_MOTOR = 6;
-		
-		//VALUES:
-		final boolean AGITATOR_IS_INVERTED = false;
-		final double AGITATOR_SPEED = 0.8;
-		
-		final boolean CONVEYOR_IS_INVERTED = false;
-		final double CONVEYOR_SPEED = 0.8;
-	}
-	
-	public interface Climber {
-		//PORTS:
-		final int CLIMBER_MOTOR = 10;
-		
-		//VALUES:
-		final boolean IS_INVERTED = false;
-		final double CLIMBER_SPEED = 0.8;
-	}
-	
-	public interface GearCollector {
-		//PORTS:
-		final int GEAR_SOLENOID = 0;
-	}
-	
-	public interface Pixy {
-		//PORTS:
-		final SerialPortType IR_PORT_TYPE = SerialPortType.SERIAL_MXP;
-		final SerialPortType COLOR_PORT_TYPE = SerialPortType.I2C_ONBOARD;
-		final int IR_I2C_VALUE = 0x5; // Only needed if using I2C
-		final int COLOR_I2C_VALUE = 0x5;
-		
-		//VALUES:
-		final int CAM_WIDTH = 318; //in pixels
-		final int CAM_HEIGHT = 198;//in pixels
-		final int CAM_X_ANGLE = 80;//in degrees
-		final int CAM_Y_ANGLE = 40;//in degrees
-		final int BAUDRATE = 19200;
-	}
-	
-	/** Serial port types that make it so that serial objects only need their port value changed in RobotMap */
-	public static enum SerialPortType{
-		SERIAL_ONBOARD, SERIAL_MXP, I2C_ONBOARD, I2C_MXP
-	}
-	
-	public static class Joystick {
-		//PORTS:
-		public static final int PORT_NUMBER = 0;
-		public static final int LEFT_Y = 1;
-		public static final int RIGHT_Y = 5;
-		public static final int LEFT_X = 0;
-		public static final int RIGHT_X = 4;
-	}
-	
-	public interface LIDAR {
-		//PORTS:
-		final SerialPort.Port PORT = SerialPort.Port.kUSB;
-		
-		//VALUES:
-		final int BAUD = 9600;
-	}
-	
-	public interface Flashlight {
-		final int GEAR_LIGHT_PORT = 1;
-		final int SHOOT_LIGHT_PORT = 0;
 	}
 }
