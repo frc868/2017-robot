2be8019938298a5694ed1227753589cbf5a99670
Clayton <cjdetke@yahoo.com>
2017-02-20 11:14:59 -0500

    Made subpackage shooter, moved commands there, made some basic ones, & updated references.

--- src/org/usfirst/frc/team868/robot/commands/groups/FeedAndShootCommandGroup.java
+++ src/org/usfirst/frc/team868/robot/commands/groups/FeedAndShootCommandGroup.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team868.robot.commands.groups;
 
 import org.usfirst.frc.team868.robot.commands.subsystems.AgitatorCommand;
-import org.usfirst.frc.team868.robot.commands.subsystems.ShootCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.ShooterFeederCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.shooter.ShootCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.turret.RotateUsingIRPixy;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
---
+++ src/org/usfirst/frc/team868/robot/commands/subsystems/shooter/StopShooting.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team868.robot.commands.subsystems.shooter;
+
+import org.usfirst.frc.team868.robot.commands.subsystems.AgitatorCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.ShooterFeederCommand;
+import org.usfirst.frc.team868.robot.subsystems.AgitatorSubsystem;
+import org.usfirst.frc.team868.robot.subsystems.FeederSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class StopShooting extends CommandGroup {
+
+    public StopShooting() {
+    	if(AgitatorSubsystem.getInstance().getState() != AgitatorSubsystem.State.OFF){//Deactivates agitator
+    		addSequential(new AgitatorCommand());
+    	}
+    	if(FeederSubsystem.getInstance().isFeederOn()){//Deactivates feeder
+    		addSequential(new ShooterFeederCommand());
+    	}
+    	addSequential(new ShootUsingVoltage(0));//Deactivates shooter
+    	
+    	//Should we also stop the turret from rotating to whatever target it finds?
+    }
+}
