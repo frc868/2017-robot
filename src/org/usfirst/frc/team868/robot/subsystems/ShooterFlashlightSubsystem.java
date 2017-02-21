683d4fa1b5a1ad75f146a37c7a5b7e3b4a0c04fe
HMPerson1 <hmperson1+github@gmail.com>
2017-02-21 18:58:29 -0500

    Fix ShooterFlashlightSystem.isOn()

--- src/org/usfirst/frc/team868/robot/subsystems/ShooterFlashlightSubsystem.java
+++ src/org/usfirst/frc/team868/robot/subsystems/ShooterFlashlightSubsystem.java
@@ -54,7 +54,7 @@ public class ShooterFlashlightSubsystem extends Subsystem {
      * @return current status of the flashlight.
      */
     public boolean isOn() {
-    	return flashlight.get() == Relay.Value.kOn;
+    	return isOn;
     }
 
     /**
