686fc9a5b48f2bc115d75e0b5abfcefff4f90cb5
Evan Kenyon <evan.m.kenyon@gmail.com>
2017-02-21 17:36:09 -0500

    Set up all of the Auton Command Groups and the values, angles and distances needed for them

--- src/org/usfirst/frc/team868/robot/commands/auton/GearToNeutralCommandGroup.java
+++ src/org/usfirst/frc/team868/robot/commands/auton/GearToNeutralCommandGroup.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team868.robot.commands.auton;
 
+import org.usfirst.frc.team868.robot.RobotMap;
 import org.usfirst.frc.team868.robot.commands.AutonChooser.StartingPoint;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.DriveDistance;
 import org.usfirst.frc.team868.robot.commands.subsystems.drive.TurnToAngle;
@@ -14,36 +15,36 @@ public class GearToNeutralCommandGroup extends CommandGroup {
     public GearToNeutralCommandGroup(StartingPoint selected) {
     	switch(selected) {
 	    	case B1:
-	    		addSequential(new DriveDistance(-75));
-	    		addSequential(new TurnToAngle(45));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(-RobotMap.AutonValues.distanceToBackupMoreFromPos1orPos3));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	case B2:
 	    		addSequential(new TurnToAngle(90));
-	    		addSequential(new DriveDistance(300));
-	    		addSequential(new TurnToAngle(-90));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceAcrossFromPos2));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	case B3:
-	    		addSequential(new DriveDistance(-75));
-	    		addSequential(new TurnToAngle(-45));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(-RobotMap.AutonValues.distanceToBackupMoreFromPos1orPos3));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	case R1:
-	    		addSequential(new DriveDistance(-75));
-	    		addSequential(new TurnToAngle(-45));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(-RobotMap.AutonValues.distanceToBackupMoreFromPos1orPos3));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	case R2:
 	    		addSequential(new TurnToAngle(-90));
-	    		addSequential(new DriveDistance(300));
-	    		addSequential(new TurnToAngle(90));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceAcrossFromPos2));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	case R3:
-	    		addSequential(new DriveDistance(-75));
-	    		addSequential(new TurnToAngle(45));
-	    		addSequential(new DriveDistance(1000));
+	    		addSequential(new DriveDistance(-RobotMap.AutonValues.distanceToBackupMoreFromPos1orPos3));
+	    		addSequential(new TurnToAngle(0));
+	    		addSequential(new DriveDistance(RobotMap.AutonValues.distanceToNeutralZoneFromGear));
 	    		break;
 	    	default: 
 	    		break;
