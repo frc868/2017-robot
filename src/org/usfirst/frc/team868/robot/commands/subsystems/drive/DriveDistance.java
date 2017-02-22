1c8d9a385e42874044ca416c2c7810c334395d8e
Caleb <karagenit@outlook.com>
2017-02-21 19:39:46 -0500

    Fix DriveDistance Command
    Now Finishes

--- src/org/usfirst/frc/team868/robot/commands/subsystems/drive/DriveDistance.java
+++ src/org/usfirst/frc/team868/robot/commands/subsystems/drive/DriveDistance.java
@@ -81,7 +81,7 @@ public class DriveDistance extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return false;
+    	return control.getError() < 5;
     }
 
     // Called once after isFinished returns true
