7788fa5374c9aa9b3a41595bbce7c02b47283f9f
Calvin Henry <developer@10.35.16.221>
2017-03-03 23:26:02 -0500

    Updated toggleFeeder&Agitator to account for feeder & agitator not in sync

--- src/org/usfirst/frc/team868/robot/commands/groups/ToggleFeederAndAgitator.java
+++ src/org/usfirst/frc/team868/robot/commands/groups/ToggleFeederAndAgitator.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team868.robot.commands.groups;
 
 import org.usfirst.frc.team868.robot.commands.subsystems.AgitatorCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.ShooterFeederCommand;
+import org.usfirst.frc.team868.robot.subsystems.AgitatorSubsystem;
+import org.usfirst.frc.team868.robot.subsystems.FeederSubsystem;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -11,7 +13,19 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ToggleFeederAndAgitator extends CommandGroup {
 
     public ToggleFeederAndAgitator() {
-    	addParallel(new AgitatorCommand());
-    	addParallel(new ShooterFeederCommand());
+    	//If agitator is running, turns off agitator, and if feeder is running, turns off feeder.
+    	if(AgitatorSubsystem.getInstance().getState() != AgitatorSubsystem.State.OFF){
+    		addParallel(new AgitatorCommand());
+    		if(FeederSubsystem.getInstance().getState() != FeederSubsystem.State.OFF){
+    			addParallel(new ShooterFeederCommand());
+    		}
+    	//If agitator isn't running, but feeder is, turns off feeder.
+    	}else if(FeederSubsystem.getInstance().getState() != FeederSubsystem.State.OFF){
+			addParallel(new ShooterFeederCommand());
+		//If both aren't running, turns them both on.
+    	}else{
+    		addParallel(new AgitatorCommand());
+			addParallel(new ShooterFeederCommand());
+    	}
     }
 }
