8065decbf42475ca33b07746ff938ccd992cfaaf
Caleb <karagenit@outlook.com>
2017-03-16 17:02:42 -0400

    Add DriveSubsystem Power Manager Thread

---
+++ src/org/usfirst/frc/team868/robot/subsystems/PowerDrawSubsystem.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team868.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class PowerDrawSubsystem extends Subsystem { //TODO does this really need to be a Subsystem??
+
+	PowerDistributionPanel pdp;
+	DriverStation ds;
+	
+	public PowerDrawSubsystem() {
+		pdp = new PowerDistributionPanel();
+		ds = DriverStation.getInstance();
+	}
+	
+	public double getTotalCurrent() {
+		return pdp.getTotalCurrent();
+	}
+	
+	public double getDriveCurrent() {
+		return  pdp.getCurrent(0) +
+				pdp.getCurrent(1) +
+				pdp.getCurrent(14) +
+				pdp.getCurrent(15);
+	}
+	
+	public double getVoltage() {
+		return pdp.getVoltage();
+	}
+	
+	public double getVoltageRemote() {
+		return ds.getBatteryVoltage();
+	}
+	
+	public boolean isLocalBrownout() {
+		return getVoltage() < 6.8;
+	}
+	
+	public boolean isRemoteBrownout() {
+		return ds.isBrownedOut();
+	}
+	
+	@Override
+	protected void initDefaultCommand() {		
+	}
+
+}
