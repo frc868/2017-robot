7dbefb4a72cd94367b3497a07ce8a4b2771a3a72
Driver Station <developer@10.35.16.221>
2017-03-25 11:39:58 -0400

    fixed toggle feeder and agitator button

--- src/org/usfirst/frc/team868/robot/commands/util/ToggleFeederAndAgitator.java
+++ src/org/usfirst/frc/team868/robot/commands/util/ToggleFeederAndAgitator.java
@@ -1,26 +1,41 @@
 package org.usfirst.frc.team868.robot.commands.util;
 
 import org.usfirst.frc.team868.robot.RobotMap.State;
-import org.usfirst.frc.team868.robot.commands.subsystems.shooter.AgitatorCommand;
-import org.usfirst.frc.team868.robot.commands.subsystems.shooter.FeederCommand;
 import org.usfirst.frc.team868.robot.subsystems.AgitatorSubsystem;
 import org.usfirst.frc.team868.robot.subsystems.FeederSubsystem;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class ToggleFeederAndAgitator extends CommandGroup {
+public class ToggleFeederAndAgitator extends Command {
 
-    public ToggleFeederAndAgitator() {
+    private AgitatorSubsystem agitator;
+	private FeederSubsystem feeder;
+
+	public ToggleFeederAndAgitator() {
     	//If both are off, turns both on, else turns both off.
-    	if(AgitatorSubsystem.getInstance().getState() == State.OFF && FeederSubsystem.getInstance().getState() == State.OFF){
-    		addParallel(new AgitatorCommand(State.FORWARD));
-    		addSequential(new FeederCommand(State.FORWARD));
+    	agitator = AgitatorSubsystem.getInstance();
+    	feeder = FeederSubsystem.getInstance();
+    	requires(feeder);
+    	requires(agitator);
+    }
+    
+    @Override
+    protected void initialize(){
+    	if(feeder.getState().equals(State.OFF) && agitator.getState().equals(State.OFF)){
+    		feeder.setFeederForward();
+    		agitator.setAgitatorForward();
     	}else{
-    		addParallel(new AgitatorCommand(State.OFF));
-			addParallel(new FeederCommand(State.OFF));
+    		feeder.setFeederOff();
+    		agitator.setAgitatorOff();
     	}
     }
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
