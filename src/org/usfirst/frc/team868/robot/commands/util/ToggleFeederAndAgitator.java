ef44208f75cd724c9d5b09f450d14b4acef2d419
Clayton <cjdetke@yahoo.com>
2017-03-24 22:05:14 -0400

    Hopeful fix for toggle feeder and agitator button for operator

--- src/org/usfirst/frc/team868/robot/commands/util/ToggleFeederAndAgitator.java
+++ src/org/usfirst/frc/team868/robot/commands/util/ToggleFeederAndAgitator.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team868.robot.commands.util;
 
 import org.usfirst.frc.team868.robot.RobotMap.State;
-import org.usfirst.frc.team868.robot.commands.subsystems.shooter.AgitatorToggleCommand;
-import org.usfirst.frc.team868.robot.commands.subsystems.shooter.ShooterFeederCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.shooter.AgitatorCommand;
+import org.usfirst.frc.team868.robot.commands.subsystems.shooter.FeederCommand;
 import org.usfirst.frc.team868.robot.subsystems.AgitatorSubsystem;
 import org.usfirst.frc.team868.robot.subsystems.FeederSubsystem;
 
@@ -14,19 +14,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ToggleFeederAndAgitator extends CommandGroup {
 
     public ToggleFeederAndAgitator() {
-    	//If agitator is running, turns off agitator, and if feeder is running, turns off feeder.
-    	if(AgitatorSubsystem.getInstance().getState() != State.OFF){
-    		addParallel(new AgitatorToggleCommand());
-    		if(FeederSubsystem.getInstance().getState() != State.OFF){
-    			addParallel(new ShooterFeederCommand());
-    		}
-    	//If agitator isn't running, but feeder is, turns off feeder.
-    	}else if(FeederSubsystem.getInstance().getState() != State.OFF){
-			addParallel(new ShooterFeederCommand());
-		//If both aren't running, turns them both on.
+    	//If both are off, turns both on, else turns both off.
+    	if(AgitatorSubsystem.getInstance().getState() == State.OFF && FeederSubsystem.getInstance().getState() == State.OFF){
+    		addParallel(new AgitatorCommand(State.FORWARD));
+    		addSequential(new FeederCommand(State.FORWARD));
     	}else{
-    		addParallel(new AgitatorToggleCommand());
-			addParallel(new ShooterFeederCommand());
+    		addParallel(new AgitatorCommand(State.OFF));
+			addParallel(new FeederCommand(State.OFF));
     	}
     }
 }
