bef1d6d9ec2360735cff5a9f5e6e8c126ff43e09
Driver Station <developer@10.35.16.221>
2017-04-08 16:23:54 -0400

    Improved IR tracking that needs to be tested, basically simplified PID to just use a P

--- src/org/usfirst/frc/team868/robot/commands/subsystems/turret/TurretIRLockToTarget.java
+++ src/org/usfirst/frc/team868/robot/commands/subsystems/turret/TurretIRLockToTarget.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team868.robot.commands.subsystems.turret;
 
+import org.usfirst.frc.team868.robot.RobotMap;
 import org.usfirst.frc.team868.robot.subsystems.IRPixySubsystem;
 import org.usfirst.frc.team868.robot.subsystems.TurretRotationSubsystem;
 
@@ -13,6 +14,8 @@ public class TurretIRLockToTarget extends Command {
 	TurretRotationSubsystem turret;
 	IRPixySubsystem pixy;
 	double xDegreesOff;
+	final double MARGIN = .1;
+	final double MULTIPLIER = .2;
 	
     public TurretIRLockToTarget() {
     	pixy = IRPixySubsystem.getInstance();
@@ -27,7 +30,13 @@ public class TurretIRLockToTarget extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	xDegreesOff = pixy.getTarget().getXAngleOffFromCenter();
-    	turret.setAngle(turret.getAngle()+xDegreesOff);
+    	if(xDegreesOff > MARGIN){
+    		turret.setPower(RobotMap.Turret.MIN_VOLTAGE+(xDegreesOff*MULTIPLIER));
+    	}else if(xDegreesOff < -MARGIN){
+    		turret.setPower(-RobotMap.Turret.MIN_VOLTAGE-(xDegreesOff*MULTIPLIER));
+    	}else{
+    		turret.setPower(0);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
