2fad5c50cb9ae80e4b45029cfa9a476dfb4f5b42
Caleb Smith <karagenit@outlook.com>
2017-04-24 14:53:27 -0400

    Redo Gear To Shoot Auton

--- src/org/usfirst/frc/team868/robot/commands/auton/GearToShootAuton.java
+++ src/org/usfirst/frc/team868/robot/commands/auton/GearToShootAuton.java
@@ -8,11 +8,9 @@ import org.usfirst.frc.team868.robot.commands.subsystems.drive.TurnByAngleGyro;
 import org.usfirst.frc.team868.robot.commands.subsystems.shooter.AgitatorCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.shooter.FeederCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.shooter.ShooterSetSpeed;
-import org.usfirst.frc.team868.robot.commands.subsystems.shooter.ShooterSetVoltageCommand;
 import org.usfirst.frc.team868.robot.commands.subsystems.turret.TurretIRLockToTarget;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -22,40 +20,38 @@ public class GearToShootAuton extends CommandGroup {
 	public GearToShootAuton(StartingPoint start) {
 		switch(start) {
 		case R2:
-			addParallel(new DriveDistance(RobotMap.AutonValues.WALL_TO_HOOK));
-			addSequential(new WaitCommand(2));
+			addSequential(new DriveDistance(RobotMap.AutonValues.WALL_TO_HOOK, 2));
 			addSequential(new DriveDistance(-RobotMap.AutonValues.HOOK_BACKOFF));
-			addSequential(new TurnByAngleGyro(-60)); //75 degrees?
+			addSequential(new TurnByAngleGyro(-60, 2)); //75 degrees?
 			addParallel(new ShooterSetSpeed(84));
-			addSequential(new DriveDistance(-RobotMap.AutonValues.WALL_TO_HOOK-40));
-//			addSequential(new TurnByAngleGyro(45));
+			addSequential(new DriveDistance(-RobotMap.AutonValues.WALL_TO_HOOK-40)); //TODO make this it's own constant
 			
-			addParallel(new TurretIRLockToTarget());
-	    	addParallel(new AgitatorCommand(State.FORWARD));
+			addParallel(new TurretIRLockToTarget()); //TODO is this broken?
+	    	addParallel(new AgitatorCommand(State.FORWARD)); //TODO make this a command group
 	    	addParallel(new FeederCommand(State.FORWARD));
-	    	addSequential(new WaitCommand(8));
 	    	
-	    	addParallel(new ShooterSetVoltageCommand(0));
-	    	addParallel(new AgitatorCommand(State.OFF));
-	    	addParallel(new FeederCommand(State.OFF));
+	    	//I think this is unnecessary, as these commands should stop once teleop starts
+//	    	addSequential(new WaitCommand(8));	    	
+//	    	addParallel(new ShooterSetVoltageCommand(0));
+//	    	addParallel(new AgitatorCommand(State.OFF));
+//	    	addParallel(new FeederCommand(State.OFF)); 
 	    	break;
+	    	
 		case B2:
-			addParallel(new DriveDistance(RobotMap.AutonValues.WALL_TO_HOOK));
-			addSequential(new WaitCommand(2));
+			addSequential(new DriveDistance(RobotMap.AutonValues.WALL_TO_HOOK, 2));
 			addSequential(new DriveDistance(-RobotMap.AutonValues.HOOK_BACKOFF));
-			addSequential(new TurnByAngleGyro(60));
+			addSequential(new TurnByAngleGyro(60, 2));
 			addParallel(new ShooterSetSpeed(84));
 			addSequential(new DriveDistance(-RobotMap.AutonValues.WALL_TO_HOOK-40));
-//			addSequential(new TurnByAngleGyro(-45));
 			
 			addParallel(new TurretIRLockToTarget());
 	    	addParallel(new AgitatorCommand(State.FORWARD));
 	    	addParallel(new FeederCommand(State.FORWARD));
-	    	addSequential(new WaitCommand(8));
 	    	
-	    	addParallel(new ShooterSetVoltageCommand(0));
-	    	addParallel(new AgitatorCommand(State.OFF));
-	    	addParallel(new FeederCommand(State.OFF));
+//	    	addSequential(new WaitCommand(8));	    	
+//	    	addParallel(new ShooterSetVoltageCommand(0));
+//	    	addParallel(new AgitatorCommand(State.OFF));
+//	    	addParallel(new FeederCommand(State.OFF));
 	    	break;
 	    	
 		default:
